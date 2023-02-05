680f626bf6b95b7d18a20b981a98df082d48eda2
B1ker4nt3nd <marecsko.kornel@gmail.com>
2023-02-05 21:19:10 +0100

    not working

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 import { logSomething } from './module';
+import { Editor } from 'slate-react';
 
 logSomething('Hello World');
+
+const [value, setValue] = React.useState([
+  {
+    type: 'paragraph',
+    children: [{ text: 'A line of text in a paragraph.' }],
+  },
+]);
